a7c10fb933db52416618f704095987da0c0ec58c
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-15 15:20:18 +0530

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Even And Odd Numbers");
+for (int i = 1; i < 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("{0} IS EVEN NUMBER ", i);
+
+    }
+    else
+    {
+        Console.WriteLine("{0} is ODD NUMBER", i);
+    }
+
+
+}
+
+
